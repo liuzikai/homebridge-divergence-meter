b45d4e06341807b5f486e38cafa93c65120314c8
Zikai Liu <zikai.liu@outlook.com>
2023-05-01 00:49:47 +0200

    Implement saved random

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,8 +1,8 @@
 import {Service, PlatformAccessory, CharacteristicValue, Logger, Characteristic, PlatformConfig} from 'homebridge';
 import {DivergenceMeterPlatform} from './platform';
 import {DivergenceMeter} from './divergenceMeter';
-import storage from "node-persist";
-import path from "path";
+import storage from 'node-persist';
+import path from 'path';
 
 /**
  * Platform Accessory
@@ -15,6 +15,7 @@ export class DivergenceMeterAccessory {
   public readonly Characteristic: typeof Characteristic = this.platform.api.hap.Characteristic;
 
   private tv: Service;
+  private rand: Service;
 
   // Display names must not contain special characters like '(' and ')'
   private modes: string[] = [
@@ -23,20 +24,15 @@ export class DivergenceMeterAccessory {
 
   private meter: DivergenceMeter;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: 0,
-    Brightness: 100,
-  };
+  private savedRandom: string;
 
   constructor(
     private readonly platform: DivergenceMeterPlatform,
     private readonly accessory: PlatformAccessory,
     public readonly config: PlatformConfig,
   ) {
+
+    this.savedRandom = '0.000000';  // may be overwritten by loadPersistentStorage()
 
     // Create the BLE backend
     this.meter = new DivergenceMeter(this.log);
@@ -49,54 +45,17 @@ export class DivergenceMeterAccessory {
 
     // Get the Television service if it exists, otherwise create a new Television service
     this.tv = this.accessory.getService(this.Service.Television) || this.accessory.addService(this.Service.Television);
-
     this.setupTVService();
-
     this.setupInputSources();
 
+    // Random button
+    this.rand = this.accessory.getService(this.Service.Switch) || this.accessory.addService(this.Service.Switch);
+    this.rand.setCharacteristic(this.Characteristic.On, false);
+    this.rand.getCharacteristic(this.Characteristic.On)
+      .onSet(this.onRandSetOn.bind(this));
+
     // Restore persistent data from persistent storage
-    this.setCharacteristicsFromStorage().then();
-
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-    //   this.accessory.addService(this.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-    //   this.accessory.addService(this.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    // let motionDetected = false;
-    // setInterval(() => {
-    //   // EXAMPLE - inverse the trigger
-    //   motionDetected = !motionDetected;
-
-    //   // push the new value to HomeKit
-    //   motionSensorOneService.updateCharacteristic(this.Characteristic.MotionDetected, motionDetected);
-    //   motionSensorTwoService.updateCharacteristic(this.Characteristic.MotionDetected, !motionDetected);
-
-    //   this.log.debug('Triggering motionSensorOneService:', motionDetected);
-    //   this.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    // }, 10000);
+    this.loadPersistentStorage().then();
   }
 
   setupTVService() {
@@ -120,7 +79,7 @@ export class DivergenceMeterAccessory {
     this.tv.getCharacteristic(this.Characteristic.ConfiguredName)
       .onSet(async (value) => {
         // Persist
-        await storage.setItem('ConfiguredName', value)
+        await storage.setItem('ConfiguredName', value);
       });
   }
 
@@ -172,8 +131,9 @@ export class DivergenceMeterAccessory {
     }
   }
 
-  async setCharacteristicsFromStorage() {
+  async loadPersistentStorage() {
     await storage.init({dir: path.join(this.platform.api.user.storagePath(), 'divergence')});
+    this.savedRandom = await storage.getItem('savedRandom') || '0.000000';
     this.tv.updateCharacteristic(this.Characteristic.ConfiguredName, await storage.getItem('ConfiguredName') || 'Divergence Meter');
     this.tv.updateCharacteristic(this.Characteristic.Active, await storage.getItem('Active') || this.Characteristic.Active.INACTIVE);
     this.tv.updateCharacteristic(this.Characteristic.ActiveIdentifier, await storage.getItem('ActiveIdentifier') || 0);
@@ -197,7 +157,7 @@ export class DivergenceMeterAccessory {
       this.meter.gyroMode();
     } else if (mode === 4) {  // Saved Random
       // Use original worldline 3
-      // TODO:
+      this.meter.worldlineMode(2, this.savedRandom);
     } else if (mode <= 6) {  // Worldline 1 & 2
       // Use original worldline 1 & 2
       this.meter.worldlineMode(mode - 5, this.config.worldlines[mode - 5]);
@@ -207,6 +167,35 @@ export class DivergenceMeterAccessory {
     } else {  // Worldline 6+
       // Use original worldline 8
       this.meter.worldlineMode(7, this.config.worldlines[mode - 5]);
+    }
+  }
+
+  controlledRandomFloat(): number {
+    const {random_min, random_max} = this.config;
+    return Math.random() * (random_max - random_min) + random_min;
+  }
+
+  convertFloatToWorldline(x: number): string {
+    const y = x >= 0 ? x : -x;  // abs
+    let s = y.toFixed(8);  // extend as much as possible
+    s = s.slice(0, 8);
+
+    // Handle negative float
+    if (x < 0 && s.indexOf('.') !== -1) {
+      // Replace the part before '.' to spaces
+      s = s.replace(/^.*?\./, ' '.repeat(s.indexOf('.')) + '.');
+    }
+
+    // Users can do all kinds of wired things, such as extremely large numbers
+    // Just handling the typical floating point cases and what ever
+    if (s.length === 8) {
+      return s;
+    } else if (s.length > 8) {
+      this.log.warn('Bad worldline to display:', s);
+      return s.slice(0, 8);
+    } else {
+      this.log.warn('Bad worldline to display:', s);
+      return s.padEnd(8, ' ');
     }
   }
 
@@ -241,6 +230,24 @@ export class DivergenceMeterAccessory {
       throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
     return isOn;
+  }
+
+  async onRandSetOn(value: CharacteristicValue) {
+    const isOn = value as boolean;
+    this.log.debug('Rand set On ->', isOn);
+    if (this.tv.getCharacteristic(this.Characteristic.ActiveIdentifier).value === 4) {
+      // Saved Random
+      if (isOn) {
+        this.meter.randomMode(true);
+      } else {
+        this.savedRandom = this.convertFloatToWorldline(this.controlledRandomFloat());
+        this.log.debug('this.savedRandom =', this.savedRandom);
+        await this.handleInputSource(4);
+        await storage.setItem('savedRandom', this.savedRandom);
+      }
+    } else {
+      this.meter.randomMode(isOn);
+    }
   }
 
 }
